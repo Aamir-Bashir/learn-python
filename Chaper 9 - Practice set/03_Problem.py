ff9951c43b69aa51d48efd184462231445180dd0
Aamir Bashir <aamirbashir724@gmail.com>
2025-09-08 07:25:05 +0500

    git update

---
+++ Chaper 9 - Practice set/03_Problem.py
@@ -0,0 +1,12 @@
+#Write a program to generate multiplication tables from 2 to 20 and write it in different files.Place these files in a folder
+
+def generateTable(n):
+    table = ""
+    for i in range(1,11):
+        table += f"{n} X {i} = {n*i}\n"
+
+    with open(f"tables/table_{n}.txt", "w") as f:
+        f.write(table)
+
+for i in range (2, 21):
+    generateTable(i)
